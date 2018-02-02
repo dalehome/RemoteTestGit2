5621994ec9c0dbb46c13be8e155b3a025241dd92
Dale-Beast <dalemanus@gmail.com>
2018-02-02 12:24:59 +0000

    Add a line

--- TestGit2/src/TestinGit.java
+++ TestGit2/src/TestinGit.java
@@ -3,5 +3,6 @@ public class TestinGit {
 
 	public static void main(String[] args) {
 		System.out.println("Testing GIT from Eclise again 2");
+		System.out.println("adding a second linebule!!");
 	}
 }
