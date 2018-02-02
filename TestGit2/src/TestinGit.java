509c76bc1e8a44ff1c15355f146000d6875a56c8
U0089863 <U0089863@U0089863-TPL-A.ten.thomsonreuters.com>
2018-02-02 12:44:37 +0000

    A bit of Monty

--- TestGit2/src/TestinGit.java
+++ TestGit2/src/TestinGit.java
@@ -4,5 +4,6 @@ public class TestinGit {
 	public static void main(String[] args) {
 		System.out.println("Testing GIT from Eclise again 2");
 		System.out.println("adding a second linebule!!");
+		System.out.println( "UND NOW FOROMETHING completely different");
 	}
 }
