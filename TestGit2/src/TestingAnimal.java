28a876e53a93507bb7b54a256720904a80632c17
Dale Lenovo <numbnutsmccaskill@hotmail.com>
2018-02-03 20:49:03 +0000

    Fix bug that it wasn't printing output

--- TestGit2/src/TestingAnimal.java
+++ TestGit2/src/TestingAnimal.java
@@ -7,8 +7,8 @@ public class TestingAnimal {
 	public static void main(String[] args) {
 		Animal a1 = new Dog();
 		Animal a2 = new Cat();
-		a1.getType(); a1.getSound();
-		a2.getType(); a2.getSound();
+		System.out.println(a1.getType() + " : " + a1.getSound());
+		System.out.println(a2.getType() + " : " + a2.getSound());
 
 	}
 
